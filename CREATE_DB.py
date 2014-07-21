0d09c6d31201506fbbfd2db9f3cea05680554ae7
viktorsmari <viktorsmari@gmail.com>
2014-07-21 16:13:04 +0000

    Dummy info on create database

--- CREATE_DB.py
+++ CREATE_DB.py
@@ -10,8 +10,8 @@ print "Connection successful."
 		# status INT NOT NULL REFERENCES vb_status(id),
 		# make INT NOT NULL REFERENCES vb_make(id),
 		# color INT NOT NULL REFERENCES vb_color(id),
-		# problemdesc VARCHAR(1020),
-		# solutiondesc VARCHAR(255),
+		# problemdesc TEXT,
+		# solutiondesc TEXT,
 		# shortdesc VARCHAR(255),
 		# enteredby INT NOT NULL REFERENCES vb_users(id),
 		# assignedto INT NOT NULL REFERENCES vb_users(id),
@@ -28,7 +28,7 @@ con.execute('''
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
 		name VARCHAR(50),
 		kt VARCHAR(10),
-		email VARCHAR(50),
+		email VARCHAR(100),
 		phone VARCHAR(10),
 		address VARCHAR(100),
 		postalcode VARCHAR(8)
@@ -37,8 +37,8 @@ con.execute('''
 print "Created table vb_client!"
 
 #Create two test dummy
-con.execute('''INSERT into vb_client VALUES(1, 'Viktor', '0903843259', 'viktor@rrr.rr', '8659309', 'Bolagata', '900');''')
-con.execute('''INSERT into vb_client VALUES(2, 'Muggi', '654354489', 'mag@bleb.io', '8452652', 'Njoasdf', '255');''')
+con.execute('''INSERT into vb_client VALUES(1, 'Viktor', '0903843259', 'viktor@rrr.rr', '5659009', 'Bolagata', '900');''')
+con.execute('''INSERT into vb_client VALUES(2, 'Muggi', '654354489', 'mag@blreeb.io', '5452652', 'Njoasdf', '255');''')
 
 con.commit()
 con.close()
